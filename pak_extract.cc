982f03408c0dace1e84ac404f93aa87d4fef9757
MrSpoocy <github@mrspoocy.com>
2025-06-01 06:50:33 +0200

    add support for --output

--- pak_extract.cc
+++ pak_extract.cc
@@ -9,13 +9,14 @@
 
 namespace {
 	void usage(char const* progname) {
-		std::cerr << "Usage: " << progname << " file.pak --extract [files]" << std::endl;
+		std::cerr << "Usage: " << progname << " file.pak --extract [files] [--output directory]" << std::endl;
 		std::cerr << "       " << progname << " file.pak --dump file" << std::endl;
 		std::cerr << "       " << progname << " file.pak --list" << std::endl;
 	}
 
 	struct Extract {
 		Extract(Pak& p): m_p(p) {}
+		Extract(Pak& p, std::string const& output): m_p(p), output(output) {}
 		void operator()(std::string const& filename) { operator()(make_pair(filename, m_p[filename])); }
 		void operator()() {
 			for (Pak::files_t::const_iterator it = m_p.files().begin(); it != m_p.files().end(); ++it) operator()(*it);
@@ -28,6 +29,16 @@ namespace {
 				if (pos == std::string::npos) m_path.clear();
 				else m_path.erase(pos);
 			}
+
+			// Add output as required
+			if (!output.empty()) {
+				if (output[output.size() -1 ] != '/') {
+					output += '/';
+				}
+
+				filename.insert(0, output);
+			}
+
 			// Try to create new folders as required
 			for (std::string::size_type pos; (pos = filename.find('/', m_path.size() + 1)) != std::string::npos;) {
 				m_path = filename.substr(0, pos);
@@ -45,11 +56,14 @@ namespace {
 	  private:
 		Pak& m_p;
 		std::string m_path;
+		std::string output;
 	};
 }
 
 int main(int argc, char** argv) {
 	std::ios::sync_with_stdio(false);
+	std::string output;
+	unsigned int offset = 0;
 	if( argc < 3 ) { usage(argv[0]); return EXIT_FAILURE; }
 	try {
 		Pak p(argv[1]);
@@ -60,8 +74,17 @@ int main(int argc, char** argv) {
 			p[argv[3]].get(buf);
 			std::cout.write(&buf[0], buf.size());
 		} else if (!strcmp(argv[2],"--extract")) {
-			if (argc == 3) { (Extract(p))(); }
-			else std::for_each(argv + 3, argv + argc, Extract(p));
+			if (argc >= 5 && !strcmp(argv[argc - 2], "--output")) {
+				std::cout << "Change output path: " << argv[argc - 1] << std::endl;
+				output = argv[argc - 1];
+				offset = 2;
+			}
+
+			if (argc == 3 || (argc == 5 && offset)) {
+				 (Extract(p, output))();
+			} else {
+				std::for_each(argv + 3, argv + argc - offset, Extract(p, output));
+			}
 		} else { usage(argv[0]); return EXIT_FAILURE; }
 	} catch (std::exception& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
